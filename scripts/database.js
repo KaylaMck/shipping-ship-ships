2e8ad1b9af5b20920c2704fb55779d1bbabdeb6e
kaylamckenzie <kmckenzie82@bethelu.edu>
2024-09-27 20:33:17 -0500

    added shippingShips to database

--- scripts/database.js
+++ scripts/database.js
@@ -10,6 +10,18 @@ const database = {
         {id: 2, name: "Boaty McBoatface", dockId: 2},
         {id: 3, name: "Seawise Giant", dockId: 3},
         {id: 4, name: "Mayweather Mayflower", dockId: 4}
+    ],
+    shippingShips: [
+        {id: 1, name: "Esso Atlantic", haulerId: 1},
+        {id: 2, name: "Prairial", haulerId: 2},
+        {id: 3, name: "Palais Royal", haulerId: 3},
+        {id: 4, name: "Rivoli", haulerId: 4},
+        {id: 5, name: "Champs Elysee", haulerId: 1},
+        {id: 6, name: "Ever Ace", haulerId: 2},
+        {id: 7, name: "Nissei Maru", haulerId: 3},
+        {id: 8, name: "MSC Gulsun", haulerId: 4},
+        {id: 9, name: "HMM Rotterdam", haulerId: 1},
+        {id: 10, name: "CMA CGM Trocadero", haulerId: 2}
     ]
 }
 
@@ -17,6 +29,10 @@ export const getDocks = () => {                           //using a getter funct
     return database.docks.map(docks => ({...docks}))      //iterating through the database and making a copy of the docks object to export
 }
 
-export const getHaulers = () => {                           //retrieving the hauler objects from the database
+export const getHaulers = () => {                           //getting the hauler objects from the database
     return database.haulers.map(haulers => ({...haulers}))     //iterating through the hauler objects and creating a copy to be exported
+}
+
+export const getShippingShips = () => {                                             //using getter function to retrieve the shippingShips object from the database
+    return database.shippingShips.map(shippingShips => ({...shippingShips}))        //iterating through shippingShips object and creating a copy to be exported
 }
